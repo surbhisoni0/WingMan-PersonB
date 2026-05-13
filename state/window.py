1d848e24823eefb81f54e0546e1d05fd0ec28bc6
surbhisoni0 <surbhi0140@gmail.com>
2026-05-13 22:43:34 +0530

    Create window.py

---
+++ state/window.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class TelemetryWindow:
+
+    def __init__(self, size=20):
+        self.window = deque(maxlen=size)
+
+    def add(self, state):
+        self.window.append(state)
+
+    def latest(self):
+        return self.window[-1]
+
+    def average_speed(self):
+
+        if not self.window:
+            return 0
+
+        return sum(
+            s["speed"] for s in self.window
+        ) / len(self.window)
